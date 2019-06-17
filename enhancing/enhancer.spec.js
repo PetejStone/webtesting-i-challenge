0df3f281d2177415f25655df1dcf19936bd0e16b
PetejStone <petejstoney@gmail.com>
2019-06-17 13:25:16 -0700

    added one working repair test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+    describe('repair()', () => {
+        it('should restore durability to 100', () => {
+            const item = {
+                durability: 75
+            }
+            expect(enhancer.repair(item).durability).toBe(100)
+        })
+    })
+})
