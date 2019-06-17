1816946475a2ffb26ba52eb91842f4dd3ef15685
PetejStone <petejstoney@gmail.com>
2019-06-17 13:43:57 -0700

    abstracted out repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,13 +1,17 @@
-const enhancer = require('./enhancer.js');
+//const enhancer = require('./enhancer.js');
+const {repair} = require('./enhancer.js')
 // test away!
 
 describe('enhancer.js', () => {
     describe('repair()', () => {
         it('should restore durability to 100', () => {
-            const item = {
-                durability: 75
-            }
-            expect(enhancer.repair(item).durability).toBe(100)
+            // const item = {
+            //     durability: 75
+            // }
+            expect(repair({durability: 72 }).durability).toBe(100)
+            expect(repair({durability: 172 }).durability).toBe(100)
+            expect(repair({durability: -72 }).durability).toBe(100)
+            expect(repair({durability: 0 }).durability).toBe(100)
         })
     })
 })
