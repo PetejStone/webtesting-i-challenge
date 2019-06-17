3b1f22bebc8af728273eebf062153db46e87709f
PetejStone <petejstoney@gmail.com>
2019-06-17 14:36:54 -0700

    added fail test description

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -21,6 +21,14 @@ describe('enhancer.js', () => {
         it('should increase enhancement by 1 and cap at 20 w/o changing durability', () => {
             
             expect(succeed({enhancement: 3 }).enhancement).toBe(3 + 1)
+            expect(succeed({enhancement: 19 }).enhancement).toBe(19 + 1)
+            expect(succeed({enhancement: 20 }).enhancement).toBe(20)
+        })
+    })
+
+    describe('fail()', () => {
+        it('should -5 from durability if enhancement < 15, or -10 if > 15. AND -1 from enhancement if enhancement > 16', () => {
+         
         })
     })
 })
