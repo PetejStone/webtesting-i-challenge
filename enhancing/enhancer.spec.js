26386658371a14490b93d3f3e19facf2ba1ef168
PetejStone <petejstoney@gmail.com>
2019-06-17 14:21:18 -0700

    added first succeed test for enhacment

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,5 +1,5 @@
 //const enhancer = require('./enhancer.js');
-const {repair} = require('./enhancer.js')
+const {repair, succeed} = require('./enhancer.js')
 // test away!
 
 describe('enhancer.js', () => {
@@ -12,6 +12,15 @@ describe('enhancer.js', () => {
             expect(repair({durability: 172 }).durability).toBe(100)
             expect(repair({durability: -72 }).durability).toBe(100)
             expect(repair({durability: 0 }).durability).toBe(100)
+            
+        })
+    })
+
+
+    describe('success()', () => {
+        it('should increase enhancement by 1 and cap at 20 w/o changing durability', () => {
+            
+            expect(succeed({enhancement: 3 }).enhancement).toBe(3 + 1)
         })
     })
 })
